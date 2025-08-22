6f180870b6f10864b1e67a3518f22e98601e7665
keerthana.p <keerthana_pandurangan@comcast.com>
2025-08-22 06:42:46 +0100

    RDKBACCL-1040 : Automate udhcpc steps in bootup script for ext builds
    Reason for change: Need to run udhcpc to get ip of brlan0 for
    connected clients internet connectivity
    Test Procedure: Tested with daisy chain
    Risks: Low
    
    Change-Id: Id7a05583e8e733827c1ee9677efe123c0965e0af
    Signed-off-by: keerthana.p <keerthana_pandurangan@comcast.com>

--- recipes-ccsp/unified-wifi-mesh/files/setup_ext_pre.sh
+++ recipes-ccsp/unified-wifi-mesh/files/setup_ext_pre.sh
@@ -17,3 +17,9 @@ do
   channel_exists=`iw dev | grep $al_mac_addr  -A 4 | grep channel | wc -l`
   ssid_exists=`iw dev | grep $al_mac_addr -A 4 | grep ssid | wc -l`
 done
+
+#Run udhcpc to get ipaddr of brlan0 interface for connected clients internet connectivity
+brlan0_ip_addr=`ifconfig brlan0 | grep "inet addr:" | cut -d ':' -f2 | cut -d ' ' -f1 | wc -l`
+if [ "$brlan0_ip_addr" = 0 ]; then
+udhcpc -i  brlan0 -q
+fi
